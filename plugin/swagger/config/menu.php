99fe76851ae8eafa5a217b8e464c2c102ceff004
hongson <goodecba@gmail.com>
2023-08-15 21:56:42 +0800

    test

---
+++ plugin/swagger/config/menu.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'title' => '开发文档',
+        'key' => 'ziyoren-swagger',
+        'icon' => 'layui-icon-code-circle',
+        'weight' => 0,
+        'type' => 1,
+        'href' => '/app/swagger',
+    ]
+];
