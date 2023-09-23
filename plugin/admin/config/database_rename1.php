abca990c56fb118179f8bf1ec25c64ff2bf7e22b
hongson <goodecba@gmail.com>
2023-09-23 22:43:06 +0800

    add

---
+++ plugin/admin/config/database_rename1.php
@@ -0,0 +1,19 @@
+<?php
+return  [
+    'default' => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'driver'      => 'mysql',
+            'host'        => 'host',
+            'port'        => '3306',
+            'database'    => 'atest',
+            'username'    => 'hong',
+            'password'    => 'test',
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_general_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
+        ],
+    ],
+];
